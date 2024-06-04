161d1dedd033572f6a0310609c38dd13a2c317c0
SandyArdiyan <sanardiyan881@gmail.com>
2024-06-04 17:40:46 +0700

    3

--- ioiFile/ioiFile.cpp
+++ ioiFile/ioiFile.cpp
@@ -15,3 +15,12 @@ int main() {
 
 	//unlimited loop untuk menulis
 	while (true) {
+		cout << "_";
+		// mendapatkan setiap karakter dalam satu baris
+		getline(cin, baris);
+		//loop akan berhenti jika memasukan karakter q
+		if (baris == "q") break;
+		// menulis dan memasukan nilai dari 'baris' ke dalam file
+		outfile << baris << endl;
+	}
+	
