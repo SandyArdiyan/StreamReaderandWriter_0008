6ba817256beffca3ec0526e9144dad49d3f282da
SandyArdiyan <sanardiyan881@gmail.com>
2024-06-04 18:45:12 +0700

    7

---
+++ ExeceptionStandar/ExeceptionStandar.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <exception>
+// untuk obyek exception yang akan di gunakan
+#include <array>
+//untuk obyek array yang akan kita gunakan 
+using namespace std;
+
+int main()
+{
+	
