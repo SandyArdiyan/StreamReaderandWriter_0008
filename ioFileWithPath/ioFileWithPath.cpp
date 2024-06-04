9e133732fcce4b933c894f5b7bf4318e49030d2b
SandyArdiyan <sanardiyan881@gmail.com>
2024-06-04 18:16:49 +0700

    6

--- ioFileWithPath/ioFileWithPath.cpp
+++ ioFileWithPath/ioFileWithPath.cpp
@@ -27,4 +27,30 @@ int main() {
 	// selesai dalam menulis sekarang tutup filenya
 	outfile.close();
 
-	
+	// Membuka file dalam mode membaca
+	ifstream infile;
+	// Menunjukan ke sebuah file
+	outfile.close();
+
+
+	ifstream infile;
+	// menunjuk ke sebuah file
+	infile.open(NamaFile + ".txt", ios::in);
+
+	cout << endl << ">= Membuka dan membaca file " << endl;
+	// jika file ada maka
+	if (infile.is_open())
+	{
+		// melakukan perulangan setiap baris
+		while (getline(infile, baris))
+		{
+			// dan tampilkan di sini
+			cout << baris << 'n';
+		}
+		// tutup file tersebut setelah selesai
+		infile.close();
+	}
+	// jika tidak di temukan file maka akan menampilkan ini
+	else cout << "Unable to open file";
+	return 0;
+}
