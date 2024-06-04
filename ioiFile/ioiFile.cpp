16a9959f96095b36341480d4ff15188672f3470c
SandyArdiyan <sanardiyan881@gmail.com>
2024-06-04 17:23:40 +0700

    Add project files.

---
+++ ioiFile/ioiFile.cpp
@@ -0,0 +1,17 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string baris;
+
+	// membuka file dalam mode menulis
+	ofstream outfile;
+	// menunjuk ke sebuah nama file
+	outfile.open("contohfile.txt");
+
+	cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+	//unlimited loop untuk menulis
+	while (true) {
